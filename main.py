c8ae901d0563e6f316be2b60320332e866b4986c
Ghanim <galghanim1999@gmail.com>
2021-10-27 14:42:22 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+# %%
+# import libraries
+import numpy as np
+import pandas as pd
+import plotly as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv("Data/listings.csv")
+df.info()
